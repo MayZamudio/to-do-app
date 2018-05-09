258c235ca82288abc5ce46edc977eb0472190da6
Mayra Zamudio <mayperal89@gmail.com>
2018-05-09 17:22:25 +0000

    took away inline js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-function onSubmit() {
-  let input = document.getElementById('input');
+
+
+var myButton = document.getElementById("clickButton");
+var myText = document.getElementById("helloText");
+
+myButton.addEventListener('click', doSomething, false)
+
+function doSomething() {
+	let input = document.getElementById('input');
 
   
   let ul = document.getElementById('list');
@@ -7,7 +14,10 @@ function onSubmit() {
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
 
+
   input.value = ""
 };
 
+
 const body = document.querySelector('body');
+document.getElementById("myForm").addEventListener("submit", doSomething);
